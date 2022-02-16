f3fc4cea9c68cbd473a56b8736e919154de2cf9c
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-17 05:11:21 +0530

    Add project files.

---
+++ Line_Comparision/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Line_Comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Program!");
+        }
+    }
+}
