ee2519492155bd881f8a7e020c430d1a0f2d7964
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-17 05:32:11 +0530

    Cartision Method

--- Line_Comparision/Program.cs
+++ Line_Comparision/Program.cs
@@ -1,13 +1,28 @@
 ﻿
 using System;
 
-namespace Line_Comparision
+using System.Collections.Generic;
+
+namespace UC1_Cartesian
 {
-    class Program
+    class LineComparison
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Line Comparison Program!");
+            LineComparison.getCartesian();
+        }
+        static void getCartesian()
+        {
+            double x1, x2, y1, y2;
+            double length = 0;
+            Console.WriteLine("Enter point 1 (x1,y1): ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter point 2 (x2,y2): ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            length = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            Console.WriteLine("The Distance between two points are:" + length);
         }
     }
 }
